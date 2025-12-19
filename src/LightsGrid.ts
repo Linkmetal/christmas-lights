07a18c737d4e0dc287021020520d38825496ccbb
Carlos Díaz <linkmetal18@gmail.com>
2025-12-19 17:08:21 +0000

    red: should return 1.000.000 after turning on all lights

--- src/LightsGrid.ts
+++ src/LightsGrid.ts
@@ -1,4 +1,8 @@
 export class LightsGrid {
+  turnOn(arg0: number, arg1: number, arg2: number, arg3: number) {
+    throw new Error("Method not implemented.")
+  }
+
   turnedOnLights() {
     return 0
   }
--- src/main.spec.ts
+++ src/main.spec.ts
@@ -7,4 +7,12 @@ describe("LightsGrid", () => {
 
     expect(lightsGrid.turnedOnLights()).toBe(0)
   })
+
+  it("should return 1.000.000 after turning on all lights", () => {
+    const lightsGrid = new LightsGrid()
+
+    lightsGrid.turnOn(0, 0, 999, 999)
+
+    expect(lightsGrid.turnedOnLights).toBe(1_000_000)
+  })
 })
