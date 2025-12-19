1bd2f9ab496ba6d800aaf7791d1459209381972d
Carlos Díaz <linkmetal18@gmail.com>
2025-12-19 17:33:49 +0000

    refactor: use constants

--- src/LightsGrid.ts
+++ src/LightsGrid.ts
@@ -10,16 +10,16 @@ export class LightsGrid {
   }
 
   turnOn(xFirstCoordinate: number, yFirstCoordinate: number, xSecondCoordinate: number, ySecondCoordinate: number) {
-    // this.grid = Array.from(new Array(1000), () => Array.from(new Array(1000), () => this.TURNED_ON))
     for (let i = xFirstCoordinate; i <= xSecondCoordinate; i++) {
       for (let j = yFirstCoordinate; j <= ySecondCoordinate; j++) {
-        this.grid[i][j] = true
+        this.grid[i][j] = this.TURNED_ON
       }
     }
   }
 
   turnedOnLights() {
     let count = 0
+
     for (const row of this.grid) {
       for (const cell of row) {
         if (cell) count++
