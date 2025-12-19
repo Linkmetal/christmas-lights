5a06ab7be63c381a37a1ebce20a96b7ae66f3ada
Carlos Díaz <linkmetal18@gmail.com>
2025-12-19 17:24:11 +0000

    refactor: should return 1.000.000 after turning on all lights

--- src/LightsGrid.ts
+++ src/LightsGrid.ts
@@ -1,12 +1,16 @@
 export class LightsGrid {
   private grid: boolean[][]
 
+  private TURNED_OFF = false
+
+  private TURNED_ON = true
+
   constructor() {
-    this.grid = Array.from(new Array(1000), () => Array.from(new Array(1000), () => false))
+    this.grid = Array.from(new Array(1000), () => Array.from(new Array(1000), () => this.TURNED_OFF))
   }
 
   turnOn(xFirstCoordinate: number, yFirstCoordinate: number, xSecondCoordinate: number, ySecondCoordinate: number) {
-    this.grid = Array.from(new Array(1000), () => Array.from(new Array(1000), () => true))
+    this.grid = Array.from(new Array(1000), () => Array.from(new Array(1000), () => this.TURNED_ON))
   }
 
   turnedOnLights() {
